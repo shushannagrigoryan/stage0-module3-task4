2a84e15719aadee650b95af10ddc1c7ab47e53a4
Shushanna <shushannagrigoryan7@gmail.com>
2022-07-20 19:49:30 +0400

    1

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,15 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int c1 = 99;
+        int c2 = 104;
+        int c3 = 97;
+        int c4 =  114;
+        System.out.println((char)c1);
+        System.out.println((char)c2);
+        System.out.println((char)c3);
+        System.out.println((char)c4);
+
+
     }
 }
