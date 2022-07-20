141013bd362507d8881196ea520a840c80fce2c7
Shushanna <shushannagrigoryan7@gmail.com>
2022-07-20 20:15:39 +0400

    2

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,9 +2,10 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-        char a = 'a';
-        char b = 'b';
+        double a = 0;
+        //char a = 'a';
+        //char b = 'b';
+        double b = 0;
         double d = (9*Math.pow(a, 2) - 5*b +2 + a -7) * ((double) (a + b - 4*a*b)/2);
-
     }
 }
